e26139553329b3eda8f09a31cc4bb27ad03c5d96
Hector Corrada Bravo <hcorrada@gmail.com>
2015-12-22 05:53:26 +0000

    use new subsetted computation on matrixStats package
    From: Hector Corrada Bravo <hcorrada@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/antiProfiles@111819 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/apStats.r
+++ R/apStats.r
@@ -1,27 +1,27 @@
 #' Compute statistics used to construct antiProfile
-#' 
+#'
 #' This function calculates normal ranges of expressions and variance ratios for all probesets. To create an anti-profile,
-#' call buildAntiProfile on the output of this function 
-#' 
-#' @param e matrix of gene expression, with one column per sample 
-#' @param cl vector of normal/cancer indicators as 0/1 
+#' call buildAntiProfile on the output of this function
+#'
+#' @param e matrix of gene expression, with one column per sample
+#' @param cl vector of normal/cancer indicators as 0/1
 #' @param tiss vector of tissue types for each sample
 #' @param minL minimum number of samples of a given tissue/class to compute stats
 #' @param cutoff median absolute deviation multiplier used to determine proportion of samples within normal range of expression
 #' @param OnCutoff gene expression barcode z-score to determine if a gene is expressed
 #' @return An object of class SuccsStats
-#' 
-#' 
+#'
+#'
 #' @examples
-#'  if (require(antiProfilesData)) { 
+#'  if (require(antiProfilesData)) {
 #'    data(apColonData)
 #'    colonStats = apStats(exprs(apColonData), pData(apColonData)$Status)
 #'  }
-#' 
+#'
 #' @author Hector Corrada Bravo \email{hcorrada@@gmail.com}
 #' @seealso \code{\linkS4class{AntiProfileStats}} for the type of object returned. \code{\link{buildAntiProfile}} to construct anti-profiles
 #' with objects returned by this function.
-#' 
+#'
 #' @export
 apStats <- function(e, cl, tiss=NULL, minL=10, cutoff=5, OnCutoff=2.54)
   {
@@ -44,16 +44,16 @@ apStats <- function(e, cl, tiss=NULL, minL=10, cutoff=5, OnCutoff=2.54)
       stop("Not enough samples")
 
     message("Computing sds")
-    sd0=sapply(tIndexes0[L0>=minL],function(ind) return(matrixStats::rowSds(e[,ind])))
-    sd1=sapply(tIndexes1[L1>=minL],function(ind) return(matrixStats::rowSds(e[,ind])))
+    sd0=sapply(tIndexes0[L0>=minL],function(ind) return(matrixStats::rowSds(e, cols=ind)))
+    sd1=sapply(tIndexes1[L1>=minL],function(ind) return(matrixStats::rowSds(e, cols=ind)))
 
     SD0=rowMeans(sd0) ##im using mean but we could try median
-    SD1=rowMeans(sd1) 
-    stat=log2(SD1/SD0) ##the statistic 
+    SD1=rowMeans(sd1)
+    stat=log2(SD1/SD0) ##the statistic
 
     message("Computing ranges")
-    tMeds=sapply(tIndexes0[L0>=minL], function(ind) matrixStats::rowMedians(e[,ind]))
-    tMads=sapply(seq_along(tIndexes0[L0>=minL]), function(i) matrixStats::rowMads(e[,tIndexes0[L0>=minL][[i]]], center=tMeds[,i]))
+    tMeds=sapply(tIndexes0[L0>=minL], function(ind) matrixStats::rowMedians(e, cols=ind))
+    tMads=sapply(seq_along(tIndexes0[L0>=minL]), function(i) matrixStats::rowMads(e, cols=tIndexes0[L0>=minL][[i]], center=tMeds[,i]))
     colnames(tMads)=colnames(tMeds)
 
     meds0=matrixStats::rowMedians(tMeds)
@@ -67,7 +67,7 @@ apStats <- function(e, cl, tiss=NULL, minL=10, cutoff=5, OnCutoff=2.54)
     xx <- .succify(e,meds0,mads0,cutoff)
     tProps0 <- sapply(tIndexes0[L0>=minL], function(ind) rowMeans(xx[,ind]))
     tProps1 <- sapply(tIndexes1[L1>=minL], function(ind) rowMeans(xx[,ind]))
-    
+
     message("Making probe data frame")
     probes=data.frame(affyid=rownames(e),
       SD0=SD0,SD1=SD1,stat=stat,
